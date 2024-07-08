Fixed bug in laser_cooling_force: Negative sign error on beta term
- Corrected a negative sign error on the beta term in the laser_cooling_force module.

Next steps:
1. Investigate and resolve bugs in scattering calculations when using generated config files.
2. Fix the failing trap tests and cl_console tests.
3. Improve the new configuration file generation.
4. Implement the iterative simulation expander for scan simulation functionalities.
5. Prioritize the expansion of existing configuration files over creating new ones from scratch.
6. Create a few example .ini files to facilitate experiment running tests.
7. Combine exp_sequence unit tests with experiment running tests to streamline testing and improve

src/qlicS/laser_cooling_force.py
@@ -13,7 +13,7 @@ def get_F_0(k, sat_param, gamma) -> np.float64:
 
 def get_beta(k, sat_param, gamma, delta) -> np.float64:
     num = (
-        -(eval(configur.get("constants", "h")) / (2 * np.pi))
+        (eval(configur.get("constants", "h")) / (2 * np.pi))
         * (k**2)
         * sat_param
         * delta