676fdbc2ae31241073e1dcc8649c0984d9329e7f
mmitch12202 <mmitchell24@amherst.edu>
2024-07-10 14:55:36 -0400

    Force pylion all to be None if trap is pseudo force
    - Implemented logic to ensure pylion all is set to None when trap is pseudo force
    - Made appropriate updates to example files
    
    Next steps:
    - Benchmark against real physical cases
    - Incorporate MLOOP for machine learning optimization

--- src/qlicS/trap.py
+++ src/qlicS/trap.py
@@ -5,4 +5,5 @@ from .pylion import functions as pl_func
 
 def gen_trap_lammps(ions, type_pos):
     trap_params = {i[0]: eval(i[1]) for i in configur.items(f"trap_{type_pos}")}
-    return pl_func.linearpaultrap(trap_params, ions=ions)
+    a = not trap_params["pseudo"]
+    return pl_func.linearpaultrap(trap_params, ions=ions, all=a)
