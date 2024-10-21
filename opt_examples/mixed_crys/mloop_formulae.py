09b38ab5c829ba1ffd28843685d3ca498a161732
Paul Grajzl <paulgrajzl@gmail.com>
2024-10-21 14:17:45 -0400

    Paul Docs

--- opt_examples/mixed_crys/mloop_formulae.py
+++ opt_examples/mixed_crys/mloop_formulae.py
@@ -6,22 +6,6 @@ import numpy as np
 
 def get_run_info(experiment_dir, params) -> dict:
 <<<<<<< HEAD
-
-    def f_r(mass, ev, charge=1.60217663e-19, kappa=0.17, length=1.5e-3, freq=11.04e6, voltage=66.4, radius=1.25e-3): # defaults based on mass_selective_pruneing.ini
-        ar = -4 * charge * kappa * ev / (mass * length**2 * (2 * np.pi * freq) ** 2)
-        qr = 2 * charge * voltage / (mass * radius**2 * (2 * np.pi * freq) ** 2)
-        wr = 2 * np.pi * freq / 2 * np.sqrt(ar + qr**2 / 2)
-        f_xy = wr/2/np.pi
-        return f_xy
-
-    amu = 1.6605402e-27
-
-    o2_res = f_r(32*amu, params[2])
-    scan_seq = [78000, 100000, 125000, o2_res, 225000, 250000, 278000]
-
-    try:
-        scat = run_from_file(
-=======
     """
     Retrieves run information and calculates the cost based on simulation results.
 
@@ -46,7 +30,23 @@ def get_run_info(experiment_dir, params) -> dict:
         KeyError: If the required parameters are not found in the configuration.
     """
     if scat := run_from_file(
->>>>>>> 114d41d (docstrings)
+=======
+
+    def f_r(mass, ev, charge=1.60217663e-19, kappa=0.17, length=1.5e-3, freq=11.04e6, voltage=66.4, radius=1.25e-3): # defaults based on mass_selective_pruneing.ini
+        ar = -4 * charge * kappa * ev / (mass * length**2 * (2 * np.pi * freq) ** 2)
+        qr = 2 * charge * voltage / (mass * radius**2 * (2 * np.pi * freq) ** 2)
+        wr = 2 * np.pi * freq / 2 * np.sqrt(ar + qr**2 / 2)
+        f_xy = wr/2/np.pi
+        return f_xy
+
+    amu = 1.6605402e-27
+
+    o2_res = f_r(32*amu, params[2])
+    scan_seq = [78000, 100000, 125000, o2_res, 225000, 250000, 278000]
+
+    try:
+        scat = run_from_file(
+>>>>>>> 84bf577a2bbccf74cb86fed582767105956625a7
         optimize_mode=True,
         exp=experiment_dir,
         cloud_0_count=int(round(params[0])),
@@ -54,21 +54,33 @@ def get_run_info(experiment_dir, params) -> dict:
         iter_scan_var_seq=scan_seq,
         trap_0_endcapvoltage=params[2],
         trap_1_endcapvoltage=params[2],
+        cloud_1_count=int(round(params[1])),
+        iter_scan_var_seq=scan_seq,
+        trap_0_endcapvoltage=params[2],
+        trap_1_endcapvoltage=params[2],
         scattering_laser_scattered_ion_indices=[0, int(round(params[0]))]
+        )
         )
         non_res_list = []
         res_count = scat[4][3]
+        res_count = scat[4][3]
         for s in scat:
             if s[3] != res_count: 
+            if s[3] != res_count: 
                 non_res_list.append(s[3])
+        res_diffs = [(res_count-i) for i in non_res_list] # Optimizing Peaks
         res_diffs = [(res_count-i) for i in non_res_list] # Optimizing Peaks
         avg_diff = sum(res_diffs)/len(res_diffs)
         cost = -(avg_diff)
         bad = False
         uncer = np.std(non_res_list)
     except Exception as e:
+        uncer = np.std(non_res_list)
+    except Exception as e:
         bad = True
         cost = 0
+        uncer = 0
+        print(e)
         uncer = 0
         print(e)
     return {"cost": cost, "uncer": uncer, "bad": bad}
@@ -97,6 +109,10 @@ def return_controller(interface):
         interface,
         "neural_net",
         max_num_runs=100,
+        param_names=['Num_Be', 'Num_O2', 'V_DC'],
+        num_params=3,
+        min_boundary=[1, 1, 0,],
+        max_boundary=[20, 20, 2.5],
         param_names=['Num_Be', 'Num_O2', 'V_DC'],
         num_params=3,
         min_boundary=[1, 1, 0,],
